3a4752ea0b7440a029ed5f32a7399fb0a823a030
Jordan Juel <jjuel5@gmail.com>
2020-02-24 20:45:11 -0600

    Colors changed to solarized for Chromebook Plus

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Sauce Code Nerd Font:size=12" };
+static const char *fonts[]          = { "Fira Code:size=12" };
 static const char dmenufont[]       = "Fira Sans:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -16,13 +16,13 @@ static const char col_cyan[]        = "#005577";
 /* #include "/home/jordan/.cache/wal/colors-wal-dwm.h" */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { "#ebdbb2", "#282828", "#928374"  },
-	[SchemeSel]  = { "#ebdbb2", "#83a598", "#3c3836"  },
+	[SchemeNorm] = { "#839496", "#002b36", "#073642"  },
+	[SchemeSel]  = { "#002b36", "#839496", "#d33682"  },
 };
 
 /* tagging */
-/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "", "🌎", "", "", "", "", "", "🎵", "🎮" }; 
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "", "🌎", "", "", "", "", "", "🎵", " };*/ 
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -31,9 +31,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Surf",     NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Emacs",    NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Surf",     NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Emacs",    NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -61,11 +61,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#282828", "-nf", "#ebdbb2", "-sb", "#83a598", "-sf", "#ebdbb2", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#002b36", "-nf", "#839496", "-sb", "#839496", "-sf", "#002b36", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *firefoxcmd[] = { "firefox", NULL };
 static const char *rangercmd[] = { "st", "-e", "ranger", NULL };
 static const char *surfcmd[] = { "surf google.com", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
